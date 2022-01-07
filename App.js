ec3d43fa75080f536bbb1b1a31a3a5cde25f8370
makiika aron <aronmakiika@gmail.com>
2022-01-08 00:36:05 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Home page of my site</h1>
+            <p>This is the site that i have developed by just usinig CDN react</p>
+        </div>
+    )
+}
+
+export default App
